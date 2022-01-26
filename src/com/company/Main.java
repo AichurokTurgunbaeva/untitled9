527906afde4511df33b62bddf7e3520ea28dc15e
Notnik_KG <tasnim0800@gmail.com>
2022-01-26 21:05:48 +0600

    update

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.MissingFormatArgumentException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+                method();
+        }
+
+        public static void method () {
+                String[] names = {"Марьям","Фатима","Айзирек","Бекболот","Бектур"};
+                for (String a: names) {
+                System.out.println("I love you "+ a);
+
+        }
+
+}}
+
+
+
+
+                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
